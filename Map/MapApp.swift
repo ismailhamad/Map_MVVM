c1e8e4f0d05497169bc5fdc95268efc846238378
ismailhamad <ismailhamad2001@gmail.com>
2022-03-16 22:05:48 +0200

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by hicham on 16/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
